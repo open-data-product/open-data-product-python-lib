23b6317b12840f79d4f7e1fe44e9b284892547d0
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-04-21 19:51:33 +0200

    chore: add setuptools config

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="open-data-product-python-lib",
+    version="0.1.0",
+    description="Python library to build data products",
+    author="Open Data Product",
+    author_email="opendataproduct@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
