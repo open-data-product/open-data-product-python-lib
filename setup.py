4ebf56ee94821e9f2186bc6f6a80955577510667
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-04-22 22:35:42 +0200

    chore: add openpyxl dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "dacite>=1.9.2",
+        "openpyxl>=3.1.5",
         "pandas>=2.3.0",
         "pyyaml>=6.0.2",
         "requests>=2.32.3",
