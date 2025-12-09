739001561988edfee90cf25fac1e4a5a6aa436ce
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-12-09 01:50:38 +0100

    fix: typo

--- opendataproduct/document/data_product_manifest_updater.py
+++ opendataproduct/document/data_product_manifest_updater.py
@@ -38,7 +38,7 @@ def update_data_product_manifest(
                 repo_organization = (
                     data_product_manifest.metadata.owner.lower().replace(" ", "-")
                 )
-                repo = f"open-lifeworlds-data-product-{data_product_manifest.id}"
+                repo = f"open-data-product-{data_product_manifest.id}"
 
                 existing_output_port = (
                     next(
